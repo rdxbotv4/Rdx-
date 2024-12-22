9567a192924281b7b5701ea5cd3b321549204dd8
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:47:46 +0500

    Update and rename help.js to 0_🤞RDX _help.js

--- models/commands/0_🤞RDX _help.js
+++ models/commands/0_🤞RDX _help.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "help",
 	version: "1.0.2",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Beginner's Guide",
 	commandCategory: "system",
 	usages: "[Tên module]",
