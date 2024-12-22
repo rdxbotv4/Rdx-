16a4cc5b42c81599196e34fb2b028aca0cd04c52
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:24:14 +0500

    Update and rename date.js to 0_🤞RDX _date.js

--- models/commands/0_🤞RDX _date.js
+++ models/commands/0_🤞RDX _date.js
@@ -2,7 +2,7 @@
     name: "date",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Mention your partner",
     commandCategory: "Love",
     usages: "[@mention]",
