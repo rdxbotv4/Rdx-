f0a9e0a587f73c6f16d923ff48dfd07195b8b090
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:48:33 +0500

    Update and rename hi.js to 0_🤞RDX _hi.js

--- models/commands/0_🤞RDX _hi.js
+++ models/commands/0_🤞RDX _hi.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "hi",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "hi sticker",
   commandCategory: "QTV BOX",
   usages: "[text]",
