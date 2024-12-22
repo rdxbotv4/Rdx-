8e1479152794cd2339666243c8bfa4c5dfa2bea8
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:58:46 +0500

    Update and rename kiss.js to 0_🤞RDX _kiss.js

--- models/commands/0_🤞RDX _kiss.js
+++ models/commands/0_🤞RDX _kiss.js
@@ -3,7 +3,7 @@ module.exports.config = {
     name: "kiss",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Kiss the person you want",
     commandCategory: "Love",
     usages: "kiss [tag]",
