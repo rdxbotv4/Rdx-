73454cbf6f6951b1f326c20d68c3c6e8796d78bd
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:45:28 +0500

    Update and rename alllin.js to 0_🤞RDX _alllin.js

--- models/commands/0_🤞RDX _alllin.js
+++ models/commands/0_🤞RDX _alllin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setallbox",
   version: "1.0.9",
 	hasPermssion: 1,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Change something of group",
 	commandCategory: "box",
 	usages: "set [emoji/avt/Bname/name/QTV/rcolor] [args]",
