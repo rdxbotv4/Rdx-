d04f410d2f2d2cbd4b9066ebd8cf3c65c2546302
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:51:40 +0500

    Update and rename art.js to 0_🤞RDX _art.js

--- models/commands/0_🤞RDX _art.js
+++ models/commands/0_🤞RDX _art.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "art",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Animefy",
   commandCategory: "editing",
   usages: "reply image",
