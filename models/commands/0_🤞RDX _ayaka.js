94c7167fd4eabf1b257938e0e9fd6b5454499522
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:56:45 +0500

    Update and rename ayaka.js to 0_🤞RDX _ayaka.js

--- models/commands/0_🤞RDX _ayaka.js
+++ models/commands/0_🤞RDX _ayaka.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "ayaka",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Random ảnh ayaka",
   commandCategory: "Random-IMG",
   usages: "ayaka",
