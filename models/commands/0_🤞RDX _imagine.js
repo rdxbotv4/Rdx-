04f406597dac8d9d5d037269968216661235d6ce
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:52:54 +0500

    Update and rename imagine.js to 0_🤞RDX _imagine.js

--- models/commands/0_🤞RDX _imagine.js
+++ models/commands/0_🤞RDX _imagine.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "imagine",
   version: "1.0.",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "generate image from polination",
   commandCategory: "image",
   usages: "query",
