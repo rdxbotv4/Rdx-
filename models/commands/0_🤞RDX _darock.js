c983d779028bd7ce172d8fcaad097e6b2236529b
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:23:28 +0500

    Update and rename darock.js to 0_🤞RDX _darock.js

--- models/commands/0_🤞RDX _darock.js
+++ models/commands/0_🤞RDX _darock.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "vineboom",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "RDX_ZAIN", 
 	description: "hihihihi",
 	commandCategory: "Không cần dấu lệnh",
 	usages: "vineboom",
