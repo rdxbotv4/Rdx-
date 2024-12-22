b22f95b6348936cfb892a2278c5e345b2e0d1b61
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:30:03 +0500

    Update and rename drake.js to 0_🤞RDX _drake.js

--- models/commands/0_🤞RDX _drake.js
+++ models/commands/0_🤞RDX _drake.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "drake",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "drake",
   commandCategory: "Tiện ích",
   usages: "[text 1] | [text 2]",
