82e2cec5660f1920147641cdb0608c86dcb8f4fa
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:16:31 +0500

    Update and rename checktt.js to 0_🤞RDX _checktt.js

--- models/commands/0_🤞RDX _checktt.js
+++ models/commands/0_🤞RDX _checktt.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "checktt",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "interactive check",
 	commandCategory: "Utilities",
 	usages: "checktt",
