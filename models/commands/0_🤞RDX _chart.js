1e8ee52d1c18e8cb5da2c7f92980aca9937acf19
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:16:01 +0500

    Update and rename chart.js to 0_🤞RDX _chart.js

--- models/commands/0_🤞RDX _chart.js
+++ models/commands/0_🤞RDX _chart.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "chart",
 	version: "1.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Create interactive diagrams with top 8 groups",
 	commandCategory: "Box Chat",
 	usages: "",
