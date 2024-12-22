bfe7d9d4449cda00cb5ddd2a47045c1ba6a72409
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:27:02 +0500

    Update and rename 0_burger.js to 0_🤞RDX _burger.js

--- models/commands/0_🤞RDX _burger.js
+++ models/commands/0_🤞RDX _burger.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "burger", // Changed from "biryani" to "burger"
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Random joke image",
   commandCategory: "Image",
   usages: "burger", // Changed from "biryani" to "burger"
