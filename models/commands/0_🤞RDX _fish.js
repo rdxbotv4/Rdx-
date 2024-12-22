7cbd357185e984961be9023469f4a2a0642d745e
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:40:57 +0500

    Update and rename fish.js to 0_🤞RDX _fish.js

--- models/commands/0_🤞RDX _fish.js
+++ models/commands/0_🤞RDX _fish.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "fish",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Sell own capital",
 	commandCategory: "Economy",
     cooldowns: 5,
