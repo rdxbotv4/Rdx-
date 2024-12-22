091b4491fd9fbb34feebc4af3863126c782d8f9c
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:52:07 +0500

    Update imagesearch.js

--- models/commands/imagesearch.js
+++ models/commands/imagesearch.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "imagesearch",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Search an Image",
   commandCategory: "image",
   usages: "imagesearch [text]",
