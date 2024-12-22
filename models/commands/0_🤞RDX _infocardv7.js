77b05d1ac770fdef14cc81fc7cf03973b40a97e5
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:54:15 +0500

    Update and rename infocardv7.js to 0_🤞RDX _infocardv7.js

--- models/commands/0_🤞RDX _infocardv7.js
+++ models/commands/0_🤞RDX _infocardv7.js
@@ -4,7 +4,7 @@ module.exports.config = {
   name: "cardinfo7",
   version: "2.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "create card info",
   commandCategory: "info",
   cooldowns: 2,
