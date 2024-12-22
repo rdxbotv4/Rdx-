c972787b3e8a859c27eed1c0e46072348fdd7c79
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:54:37 +0500

    Update and rename infoip.js to 0_🤞RDX _infoip.js

--- models/commands/0_🤞RDX _infoip.js
+++ models/commands/0_🤞RDX _infoip.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "infoip",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "get info of ip address",
   usages: "[ip address]",
 	commandCategory: "...",
