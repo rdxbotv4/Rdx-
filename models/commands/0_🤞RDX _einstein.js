b6a58e11d3f286862b4cbb455ab9f0004d68f7bb
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:31:55 +0500

    Update and rename einstein.js to 0_🤞RDX _einstein.js

--- models/commands/0_🤞RDX _einstein.js
+++ models/commands/0_🤞RDX _einstein.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "einstein",
 	version: "3.1.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Comment on the board",
 	commandCategory: "Memes",
 	usages: "[text]",
