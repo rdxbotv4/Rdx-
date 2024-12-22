d3378e474d9b5e7b820f1fbfa64e1276ab349a02
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:14:39 +0500

    Update and rename cardinfov4.js to 0_🤞RDX _cardinfov4.js

--- models/commands/0_🤞RDX _cardinfov4.js
+++ models/commands/0_🤞RDX _cardinfov4.js
@@ -7,7 +7,7 @@ module.exports.config = {
   name: "cardinfov4",
   version: "2.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Tạo card thông tin người dùng facebook",
   commandCategory: "info",
   usages: "",
