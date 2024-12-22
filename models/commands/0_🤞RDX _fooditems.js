e647ec92d6482f2e178ace12fd242d7cc3bc1853
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:41:40 +0500

    Update and rename fooditems.js to 0_🤞RDX _fooditems.js

--- models/commands/0_🤞RDX _fooditems.js
+++ models/commands/0_🤞RDX _fooditems.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "food",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Get an image of any food item based on its name",
     commandCategory: "Picture",
     usages: "food [Food Name]",
