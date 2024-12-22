8b7732ba96d40cbe3252b4736d766f8d1f3ca4c1
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:57:15 +0500

    Update and rename join.js to 0_🤞RDX _join.js

--- models/commands/0_🤞RDX _join.js
+++ models/commands/0_🤞RDX _join.js
@@ -3,7 +3,7 @@ module.exports.config = {
     name: "join",
     version: "1.0.1",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Join the Bot boxes are in",
     commandCategory: "System",
     usages: "",
