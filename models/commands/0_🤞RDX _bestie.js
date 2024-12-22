323c5996149264e93fe348d4390b4d7f7f116193
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:01:58 +0500

    Update and rename bestie.js to 0_🤞RDX _bestie.js

--- models/commands/0_🤞RDX _bestie.js
+++ models/commands/0_🤞RDX _bestie.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "bestie",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: " Priyansh Rajput", 
+  credits: " RDX_ZAIN", 
   description: "Get Pair From Mention",
   commandCategory: "png",
   usages: "[@mention]",
