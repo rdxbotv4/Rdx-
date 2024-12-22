fe1629e2d4f12b2664f6a06b07468df694366dd1
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:26:56 +0500

    Update and rename dog.js to 0_🤞RDX _dog.js

--- models/commands/0_🤞RDX _dog.js
+++ models/commands/0_🤞RDX _dog.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "dog",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Xem Boss",
 	commandCategory: "Picture",
 	usages: "dog [Text]",
