a1ef43afccfd64ebec5806198840252816323bcd
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:43:15 +0500

    Update and rename gali_reply.js to 0_🤞RDX _gali_reply.js

--- models/commands/0_🤞RDX _gali_reply.js
+++ models/commands/0_🤞RDX _gali_reply.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "gali",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "RDX_ZAIN", 
 	description: "no prefix",
 	commandCategory: "no prefix",
 	usages: "abal",
