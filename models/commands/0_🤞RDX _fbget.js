6e3a9d27d3674e96f076d8b3b5c3f16aefbfb33b
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:37:28 +0500

    Update and rename fbget.js to 0_🤞RDX _fbget.js

--- models/commands/0_🤞RDX _fbget.js
+++ models/commands/0_🤞RDX _fbget.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "fbget",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Download video or record from fb",
   commandCategory: "utilities",
   usages: "audio/video [link]",
