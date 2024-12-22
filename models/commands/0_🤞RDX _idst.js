e375c42e929edb18587725b9db329cd7d5b2220d
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:51:16 +0500

    Update and rename idst.js to 0_🤞RDX _idst.js

--- models/commands/0_🤞RDX _idst.js
+++ models/commands/0_🤞RDX _idst.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "idst",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Save sticker id",
   commandCategory: "sticker",
   usages: "[reply]",
