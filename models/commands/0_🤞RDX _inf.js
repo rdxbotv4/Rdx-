f5e662ec0ee75c7007b32a3b92774979f3494e41
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:30:01 +0500

    Update and rename 0inf.js to 0_🤞RDX _inf.js

--- models/commands/0_🤞RDX _inf.js
+++ models/commands/0_🤞RDX _inf.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "info",
 	version: "1.0.1", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Admin and Bot info.",
 	commandCategory: "...",
 	cooldowns: 1,
