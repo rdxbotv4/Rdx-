5b14eee73efdd44bd1314843e8840e302fdd6da7
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:04:40 +0500

    Update and rename birthday.js to 0_🤞RDX _birthday.js

--- models/commands/0_🤞RDX _birthday.js
+++ models/commands/0_🤞RDX _birthday.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bday",
 	version: "1.0.0",
 	hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
 	description: "See admin's birthday",
   usePrefix: false,
 	commandCategory: "bday",
