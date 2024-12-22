3968ef73b341575ca921b08b0c75158d715e2738
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:24:35 +0500

    Update and rename date2.js to 0_🤞RDX _date2.js

--- models/commands/0_🤞RDX _date2.js
+++ models/commands/0_🤞RDX _date2.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "date2",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Mention your partner",
     commandCategory: "Love",
     usages: "[@mention]",
