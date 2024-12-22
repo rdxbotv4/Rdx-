43bec979516216bb8218552de0d5c3ad3e20db07
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:28:06 +0500

    Update and rename dot_spam.js to 0_🤞RDX _dot_spam.js

--- models/commands/0_🤞RDX _dot_spam.js
+++ models/commands/0_🤞RDX _dot_spam.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "dot",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "War In Chatbox",
     commandCategory: "wargroup",
     usages: "[fyt]",
