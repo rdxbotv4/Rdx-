dcd84b272a72bcce2fd18d3a65eff0a3c58ba8e9
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:59:10 +0500

    Update and rename kiss2.js to 0_🤞RDX _kiss2.js

--- models/commands/0_🤞RDX _kiss2.js
+++ models/commands/0_🤞RDX _kiss2.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "kiss2",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "for girls, Kiss the boy you want",
     commandCategory: "Love",
     usages: "kiss [tag]",
