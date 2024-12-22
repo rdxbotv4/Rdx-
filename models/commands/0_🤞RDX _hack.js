2c886c33a7064a000061195d42a4578cb497f283
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:47:15 +0500

    Update and rename hack.js to 0_🤞RDX _hack.js

--- models/commands/0_🤞RDX _hack.js
+++ models/commands/0_🤞RDX _hack.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "hack",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "hack",
   commandCategory: "hack",
   usages: "@mention",
