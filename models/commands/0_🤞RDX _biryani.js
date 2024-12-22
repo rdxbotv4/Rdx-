1a299a6f1c8670fd31baae6015e756a8b59b7e99
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:05:31 +0500

    Update and rename biryani.js to 0_🤞RDX _biryani.js

--- models/commands/0_🤞RDX _biryani.js
+++ models/commands/0_🤞RDX _biryani.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "biryani",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Random joke image",
   commandCategory: "Image",
   usages: "biryani",
