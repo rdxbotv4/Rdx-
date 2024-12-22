9a7c67725f208ff5052112ca5c342fd76fe8fdb2
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:40:51 +0500

    Update and rename adbot.js to 0_🤞RDX _adbot.js

--- models/commands/0_🤞RDX _adbot.js
+++ models/commands/0_🤞RDX _adbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
     usages: "",
