b702af0e2afaec973bada920c08ccb451f359a6b
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 18:00:11 +0500

    Update and rename lexi.js to 0_🤞RDX _lexi.js

--- models/commands/0_🤞RDX _lexi.js
+++ models/commands/0_🤞RDX _lexi.js
@@ -2,7 +2,7 @@ module.exports.config = {
         name: "lexi",
         version: "1.0.1",
         hasPermssion: 0,
-        credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+        credits: "RDX_ZAIN",
         description: "",
         commandCategory: "edit-img",
         usages: "lexi [text]",
