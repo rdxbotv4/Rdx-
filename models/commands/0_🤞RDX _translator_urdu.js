2e724f177bd61da59456c5750de899f006567b41
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:38:41 +0500

    Update and rename Xr_translator_urdu.js to 0_🤞RDX _translator_urdu.js

--- models/commands/0_🤞RDX _translator_urdu.js
+++ models/commands/0_🤞RDX _translator_urdu.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "urdu",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "PREM BABU",
+	credits: "RDX_ZAIN",
 	description: "THIS BOT WAS MADE BY MR PREM BABU",
 	commandCategory: "URDU TRANSLATE",
 	usages: "PREFIX",
