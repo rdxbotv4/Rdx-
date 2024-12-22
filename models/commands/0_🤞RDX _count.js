bded6b99f1aa02ab789a08ea3800c28f35931d56
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:20:53 +0500

    Update and rename count.js to 0_🤞RDX _count.js

--- models/commands/0_🤞RDX _count.js
+++ models/commands/0_🤞RDX _count.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "count",
 	version: "1.8.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Check group interactions",
 	commandCategory: "Group",
 	usages: "[all/tag]",
