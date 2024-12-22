086e45d2c73b3519f18773af86d8c35bfa5854ea
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:45:42 +0500

    Update groupemoji.js

--- models/commands/groupemoji.js
+++ models/commands/groupemoji.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "groupemoji",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Change your group Emoji",
 	commandCategory: "Box", 
 	usages: "groupemoji [name]", 
