dc7b256b54568221a059c9bb582b0a3583ecebef
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:43:41 +0500

    Update and rename getlink.js to 0_🤞RDX _getlink.js

--- models/commands/0_🤞RDX _getlink.js
+++ models/commands/0_🤞RDX _getlink.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "getlink",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Get the URL Download from Video, Audio is sent from the group",
 	commandCategory: "Tool",
 	usages: "getLink",
