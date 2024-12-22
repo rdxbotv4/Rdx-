bceb84502437ba033971c998776d83a9c09742dc
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:27:42 +0500

    Update and rename doof.js to 0_🤞RDX _doof.js

--- models/commands/0_🤞RDX _doof.js
+++ models/commands/0_🤞RDX _doof.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "doof",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Comment on the board ( ͡° ͜ʖ ͡°)",
 	commandCategory: "Edit-IMG",
 	usages: "doof [text]",
