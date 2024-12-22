40e883122505f9515bdc268b0ae7374068ac3880
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:35:46 +0500

    Update and rename fbautodownload.js to 0_🤞RDX _fbautodownload.js

--- models/commands/0_🤞RDX _fbautodownload.js
+++ models/commands/0_🤞RDX _fbautodownload.js
@@ -7,7 +7,7 @@ module.exports.config = {
     name: "fbautodownload",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "Priyansh Rajput",
+    credits: "RDX_ZAIN",
     description: "Downloads Facebook video from HD link provided",
     commandCategory: "utility",
     usages: "[Facebook video URL]",
