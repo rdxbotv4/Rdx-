d832318d6bc1daf90bcc99f611878cfc3cc0e303
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:55:31 +0500

    Update and rename install.js to 0_🤞RDX _install.js

--- models/commands/0_🤞RDX _install.js
+++ models/commands/0_🤞RDX _install.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "playstore",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "",
   commandCategory: "User",
   usages: "",
