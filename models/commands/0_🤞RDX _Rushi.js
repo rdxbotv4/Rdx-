1152242372efb32fad1d5a59dfc7e29ea9145427
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:36:56 +0500

    Update and rename Rushi.js to 0_🤞RDX _Rushi.js

--- models/commands/0_🤞RDX _Rushi.js
+++ models/commands/0_🤞RDX _Rushi.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "rushia",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+ credits: "RDX_ZAIN",
  description: "Random Rushia",
  commandCategory: "random-img",
  usages: "rushia",
