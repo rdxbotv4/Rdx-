86ee4fc88e3f497fe7b252ae9714a56f91d89118
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:32:26 +0500

    Update and rename elon.js to 0_🤞RDX _elon.js

--- models/commands/0_🤞RDX _elon.js
+++ models/commands/0_🤞RDX _elon.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "elon",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "RDX_ZAIN",
 	description: "elon [text]",
 	commandCategory: "edit-image",
 	usages: "elon [text]",
