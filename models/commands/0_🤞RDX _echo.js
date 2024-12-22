5206df5fd896906c86ce8e79653abb4e93b9bece
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:31:26 +0500

    Update and rename echo.js to 0_🤞RDX _echo.js

--- models/commands/0_🤞RDX _echo.js
+++ models/commands/0_🤞RDX _echo.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "echo",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "echo",
   commandCategory: "Other",
     cooldowns: 0,
