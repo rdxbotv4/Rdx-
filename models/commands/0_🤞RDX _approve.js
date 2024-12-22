43e709c36cb597eea9d3229e23e69e6b7c5f4d45
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:50:26 +0500

    Update and rename approve.js to 0_🤞RDX _approve.js

--- models/commands/0_🤞RDX _approve.js
+++ models/commands/0_🤞RDX _approve.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "approve",
 	version: "1.0.2",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "approve the gc using bots xD",
 	commandCategory: "Admin",
     cooldowns: 5
