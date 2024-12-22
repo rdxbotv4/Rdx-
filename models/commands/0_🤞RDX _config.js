584ce10a3d8633a93ee0d720ca5ebd9c7a1d1778
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:19:57 +0500

    Update and rename config.js to 0_🤞RDX _config.js

--- models/commands/0_🤞RDX _config.js
+++ models/commands/0_🤞RDX _config.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "config",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "config bot!",
 	commandCategory: "admin",
 	cooldowns: 5
