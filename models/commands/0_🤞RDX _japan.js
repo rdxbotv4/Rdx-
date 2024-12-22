71e129c1d8746c7972e6f61342ab529dd007aaeb
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 17:56:49 +0500

    Update and rename japan.js to 0_🤞RDX _japan.js

--- models/commands/0_🤞RDX _japan.js
+++ models/commands/0_🤞RDX _japan.js
@@ -2,7 +2,7 @@
   name: "japan",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Random ảnh girl",
   commandCategory: "random-img",
   usages: "japan",
