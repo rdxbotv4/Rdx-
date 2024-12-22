8d8ba650d02020760ac347f1719b007afbd34ba3
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-22 16:55:20 +0500

    Update and rename avt.js to 0_🤞RDX _avt.js

--- models/commands/0_🤞RDX _avt.js
+++ models/commands/0_🤞RDX _avt.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "avt",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "get avt id by people using it",
   commandCategory: "Công cụ",
   cooldowns: 0
