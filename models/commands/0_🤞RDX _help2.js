dc63fa17b3045b6921092a242fd38adca08b15d7
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2025-01-05 07:48:34 +0500

    Update 0_🤞RDX _help2.js

--- models/commands/0_🤞RDX _help2.js
+++ models/commands/0_🤞RDX _help2.js
@@ -71,7 +71,7 @@ module.exports.run = function ({ api, event, args, getText }) {
         i = startSlice;
         const returnArray = arrayInfo.slice(startSlice, startSlice + numberOfOnePage);
         
-        for (let item of returnArray) msg += `╰┈➤ 「 ${++i} 」${prefix}${item}\n`;
+        for (let item of returnArray) msg += `╰┈➤ 「 ${++i} 」${prefix} 𒁍 ${item}\n`;
         
         const siu = `Command list 📄\n Made by 𝗭𝗔̃𝗜̨𝗡̃𝗜̨ 𝗝𝗨𝗧𝗧💫✨ 🥀\nFor More Information type /help (command name) ✨`;
         
