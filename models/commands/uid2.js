b3909e63ee945c45edb354cec82fb3fa5f333282
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2025-01-14 03:20:03 +0500

    Update uid2.js

--- models/commands/uid2.js
+++ models/commands/uid2.js
@@ -44,9 +44,9 @@ var data = await api.getUserInfoV2(res_ID);
     if (args.join().indexOf('@') !== -1) 
       var uid = Object.keys(event.mentions) 
       var callback = () => 
-api.sendMessage({body:`${uid}`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+api.sendMessage({body:`˚₊· ͟͟͞͞➳❥${uid}`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
         () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
-    return request(encodeURI(`https://graph.facebook.com/˚₊· ͟͟͞͞➳❥${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+    return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
         () => callback()); 
 
   }
