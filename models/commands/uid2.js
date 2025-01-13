5e4df27d4585d697f43f699eec8127edc6da41e7
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2025-01-14 03:14:27 +0500

    Update uid2.js

--- models/commands/uid2.js
+++ models/commands/uid2.js
@@ -2,8 +2,8 @@ module.exports.config = {
 	name: "uid2",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "SHAAN KHAN",
-	description: "THIS BOT WAS MADE BY MR SHAAN KHAN",
+	credits: "RDX ZAIN",
+	description: "THIS BOT COMMAND MADE BY RDX",
 	commandCategory: "USER UID NUMBER",
 	cooldowns: 5
 };
@@ -24,7 +24,7 @@ const fs = global.nodemodule["fs-extra"];
       var uid = event.senderID;
       const res = await axios.get(`https://www.nguyenmanh.name.vn/api/fbInfo?id=${uid}&apikey=LV7LWgAp`);
 var name = res.data.result.name 
-        var callback = () =>  api.sendMessage({body:`${uid}`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        var callback = () =>  api.sendMessage({body:`˚₊· ͟͟͞͞➳❥${uid}`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
         () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
     return request(encodeURI(`https://graph.facebook.com/${event.senderID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
         () => callback()); 
