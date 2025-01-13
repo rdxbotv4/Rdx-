b04ec1b94339861365a9fdd4ca06351d77d68d7f
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2025-01-13 11:59:18 +0500

    Update 0_🤞RDX _admin_det.js

--- models/commands/0_🤞RDX _admin_det.js
+++ models/commands/0_🤞RDX _admin_det.js
@@ -14,9 +14,9 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
     var msg = {
-        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 👸",
+        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🤴",
         attachment: 
-fs.createReadStream(__dirname + `/noprefix/mahek.jpeg`)
+fs.createReadStream(__dirname + `/noprefix/qudus.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
